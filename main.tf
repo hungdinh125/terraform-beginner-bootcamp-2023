4f04b42a39e8eb06e8dfa9b74192dcd963df7bb0
Hung Dinh <hungdinh.dean@gmail.com>
2023-11-17 07:37:17 +0000

    #11 create a S3 and then destroy

--- main.tf
+++ main.tf
@@ -4,7 +4,15 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.26.0"
+    }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
@@ -12,8 +20,14 @@ provider "random" {
 }
 
 resource "random_string" "bucket_name" {
-  length   = 16
+  length   = 32
   special  = false
+  lower = true
+  upper = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
